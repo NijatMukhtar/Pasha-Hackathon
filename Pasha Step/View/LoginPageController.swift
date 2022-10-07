80167abf4e42bb861621f335c20380fd5a7c8e0a
Nijat Mukhtarov <mukhog97@icloud.com>
2022-10-07 23:57:54 +0400

    first creating and onboarding page

---
+++ Pasha Step/View/LoginPageController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPageController.swift
+//  Pasha Step
+//
+//  Created by Nijat Mukhtarov on 07.10.22.
+//
+
+import UIKit
+
+class LoginPageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Pasha Step/View/OnboardingPageController.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingPageController.swift
+//  Pasha Step
+//
+//  Created by Nijat Mukhtarov on 07.10.22.
+//
+
+import UIKit
+
+class OnboardingPageController: UIViewController {
+    
+    @IBOutlet weak var startButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    func setup(){
+        
+    }
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        
+    }
+    
+}

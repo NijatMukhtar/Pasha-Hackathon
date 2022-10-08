65ce83f526c1b2a1165c13770d6d3adcd6b8cf2b
Nijat Mukhtarov <mukhog97@icloud.com>
2022-10-09 00:17:16 +0400

    updates emil sicmadan evvel

--- Pasha Step/View/DetailedInformationPageController.swift
+++ Pasha Step/View/DetailedInformationPageController.swift
@@ -9,45 +9,49 @@ import UIKit
 
 
 
-class MainPageController: UIViewController {
+class DetailedInformationPageController: UIViewController {
 
-    @IBOutlet weak var button3: UIButton!
-    @IBOutlet weak var button2: UIButton!
+  
     @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var stepCounter: UILabel!
-
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
     
-    var button1Tapped = false
-    var button2Tapped = true
+    
+    var button1Tapped = true
+    var button2Tapped = false
     var button3Tapped = false
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        stepCounter.text = String(UserDefaults.standard.integer(forKey: "stepCount"))
-        
     }
     
+
+
     @IBAction func button1tapped(_ sender: Any) {
-        if button1Tapped == false{
+    
+    
+    
             button2.imageView?.image = UIImage(named: "button2untapped")
             button3.imageView?.image = UIImage(named: "button3untapped")
             button1Tapped = true
             button2Tapped = false
             button3Tapped = false
             (sender as AnyObject).setImage(UIImage(named: "but1tapped"), for: UIControl.State.normal)
-            let viewCont = storyboard?.instantiateViewController(withIdentifier: "AwardPageController") as! AwardPageController
-            viewCont.navigationItem.setHidesBackButton(true, animated: true)
-            viewCont.modalPresentationStyle = .fullScreen
-            present(viewCont, animated: false)
-
-        }
+        let viewCont = storyboard?.instantiateViewController(withIdentifier: "AwardPageController") as! AwardPageController
+        viewCont.navigationItem.setHidesBackButton(true, animated: true)
+        viewCont.modalPresentationStyle = .fullScreen
+        present(viewCont, animated: false)
+        
     
         print(button1Tapped)
     }
+
     @IBAction func button2tapped(_ sender: Any) {
-        if button2Tapped == false{
+    
+    
+    if button2Tapped == false{
             print("yes")
             button1.imageView?.image = UIImage(named: "button1untapped")
             (sender as AnyObject).setImage(UIImage(named: "but2tapped"), for: UIControl.State.normal)
@@ -55,13 +59,20 @@ class MainPageController: UIViewController {
             button2Tapped = true
             button1Tapped = false
             button3Tapped = false
+        let viewCont = storyboard?.instantiateViewController(withIdentifier: "MainPageController") as! MainPageController
+        
+        viewCont.navigationItem.setHidesBackButton(true, animated: true)
+        viewCont.modalPresentationStyle = .fullScreen
+        present(viewCont, animated: false)
         }
     
         print(button2Tapped)
     }
     
+
     @IBAction func button3tapped(_ sender: Any) {
-        if button3Tapped == false{
+    
+    if button3Tapped == false{
             print("yes")
             button1.imageView?.image = UIImage(named: "button1untapped")
             button2.imageView?.image = UIImage(named: "button2untapped")
--- Pasha Step/View/LoginPageController.swift
+++ Pasha Step/View/LoginPageController.swift
@@ -6,26 +6,97 @@
 //
 
 import UIKit
+import HealthKit
 
 class LoginPageController: UIViewController {
-
- 
+    @IBOutlet weak var mailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    let healthStore = HKHealthStore()
+    var stepCount = 0
+    
+    let mail = "test@gmail.com"
+    let password = "1234"
+    
     @IBOutlet weak var signinButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
+        
+        authorizeHealtkit()
+        getSteps()
+    }
+    
+    func setup(){
+        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        mailTextfield.leftView = paddingView
+        mailTextfield.leftViewMode = .always
+        
+        let paddingViewSecond: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        passwordTextfield.leftView = paddingViewSecond
+        passwordTextfield.leftViewMode = .always
+        
     }
     
     @IBAction func signinbuttonTapped(_ sender: Any) {
-        navigate()
+        if mailTextfield.text == mail && passwordTextfield.text == password{
+            navigate()
+        }
+        else{
+            passwordTextfield.text = ""
+            let alert = UIAlertController(title: "Alert", message: "Password is wrong, please try again.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
+    
+    func authorizeHealtkit(){
+        let read = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
+        let share = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
+        healthStore.requestAuthorization(toShare: share , read: read) { (chk, error) in
+            if(chk){
+                print("permission granted")
+            
+            }
+        }
+    }
+   
+    func getSteps(){
+        guard let sampleType = HKCategoryType.quantityType(forIdentifier: .stepCount) else{
+            return
+        }
+        let startDate = Calendar.current.startOfDay(for: Date())
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictEndDate)
+        var interval = DateComponents()
+        interval.day = 1
+        let query = HKStatisticsCollectionQuery(quantityType: sampleType, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: startDate, intervalComponents: interval)
+        query.initialResultsHandler = {
+            query, result, error in
+            
+            if let mysresult = result {
+                mysresult.enumerateStatistics(from: startDate, to: Date()) { (statistic, value) in
+                    if let count = statistic.sumQuantity(){
+                        let val = count.doubleValue(for: HKUnit.count())
+                        print(val)
+                        self.stepCount = Int(val)
+                        UserDefaults.standard.set(Int(val), forKey: "stepCount")
+                    }
+                    
+                }
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+   
     
     func navigate(){
         print("buttonTapped")
         let viewCont = storyboard?.instantiateViewController(withIdentifier: "MainPageController") as! MainPageController
+        
         viewCont.navigationItem.setHidesBackButton(true, animated: true)
         navigationController?.show(viewCont, sender: nil)
     }
     
 }
+
